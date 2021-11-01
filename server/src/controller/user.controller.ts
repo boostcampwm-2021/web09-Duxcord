a3aa1393d25e30c7ebdd62fcbfafdc7722572ba8
superLipbalm <superLipbalm@gmail.com>
2021-11-01 14:42:44 +0900

    Feat : user/signup API 추가 password bcrypt이용해 해싱 후 저장

---
+++ server/src/controller/user.controller.ts
@@ -0,0 +1,33 @@
+import { hash } from 'bcrypt';
+import { NextFunction, Request, Response } from 'express';
+import { userRepository } from '../db';
+import { User } from '../entity/user.entity';
+
+const saltRounds = 10;
+const nullCheck = (data) => data !== undefined && data !== null && data !== '';
+
+const signUp = async (req: Request, res: Response, next: NextFunction) => {
+  const { userID, username, password } = req.body;
+
+  try {
+    if (![userID, username, password].every((data) => nullCheck(data)))
+      return res.status(400).send('회원가입에 필요한 데이터일부가 누락되었습니다 않습니다.');
+
+    const isExist = await userRepository.findOne({ where: { userID: userID } });
+    if (isExist) return res.status(400).send('이미 사용중인 ID 입니다.');
+
+    const newUser = new User();
+    newUser.userID = userID;
+    newUser.userName = username;
+    newUser.password = await hash(password, saltRounds);
+    userRepository.save(newUser);
+
+    return res.status(200).send('회원가입에 성공했습니다.');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  signUp,
+};
---
+++ server/src/router/api/user.router.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+import userController from '../../controller/user.controller';
+export const userRouter = Router();
+
+userRouter.post('/signup', userController.signUp);
