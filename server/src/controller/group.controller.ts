45baf5f744256459db554a2dd847f8255d221729
seojin kim <001106ksj@gmail.com>
2021-11-02 16:18:08 +0900

    feat: 그룹 초대 코드 생성 및 저장

--- server/src/controller/group.controller.ts
+++ server/src/controller/group.controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
-import { groupRepository, userRepository } from '../db';
 
+import { groupRepository, userRepository } from '../db';
 import { Group } from '../entity/group.entity';
 
 const nullCheck = (data) => data !== undefined && data !== null && data !== '';
+const encodeBase64 = (str: string): string => Buffer.from(str, 'binary').toString('base64');
 
 const createGroup = async (req: Request, res: Response, next: NextFunction) => {
   const { groupName, groupThumbnail } = req.body;
@@ -17,9 +18,10 @@ const createGroup = async (req: Request, res: Response, next: NextFunction) => {
     newGroup.name = groupName;
     newGroup.leader = leader;
     newGroup.thumbnail = groupThumbnail;
-    newGroup.code = '123456';
+    const timestamp = new Date().getTime();
+    const newCode = encodeBase64(String(timestamp).slice(-6));
+    newGroup.code = newCode;
     await groupRepository.save(newGroup);
-
     return res.status(200).json({ code: newGroup.code });
   } catch (error) {
     next(error);
