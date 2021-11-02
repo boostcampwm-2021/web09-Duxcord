3e14289da87c0ec86dd485c71eff6de7979670ae
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-02 12:10:49 +0900

    style: BackgroundLayout style 적용

---
+++ client/src/layouts/BackgroundLayout/style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('ducks.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+export default Layout;
