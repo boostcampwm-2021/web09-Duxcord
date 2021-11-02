40e7a283339bd831411f17c98e77df84635ca045
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-02 14:15:37 +0900

    feat : 회원가입 태그 생성(기본 레이아웃)

---
+++ client/src/pages/SignUp/index.tsx
@@ -0,0 +1,37 @@
+import React,{useState} from 'react';
+import BackgroundLayout from '../../layouts/BackgroundLayout'
+import {
+} from './style';
+
+function SignUp() {
+
+  return (
+    <BackgroundLayout>
+      <div>
+        <h1>계정 만들기</h1>
+        <div>
+          <label htmlFor="user_id">아이디</label>
+          <input id="user_id" type="text" />
+          <p>이미 사용중인 아이디 입니다.</p>
+        </div>
+        <div>
+          <label htmlFor="user_name">사용자명</label>
+          <input id="user_name" type="text" />
+        </div>
+        <div>
+        <label htmlFor="user_password">비밀번호</label>
+          <input id="user_password" type="password" />
+          <p>비밀먼호는 특수문자 포함 6자 이상으로 해주세요.</p>
+        </div>
+        <div>
+          <button>
+              <p>가입하기</p>
+          </button>
+          <div>이미 계정이 있으신가요?</div>
+        </div>
+      </div>
+    </BackgroundLayout>
+  );
+}
+
+export default SignUp;
