6ba44083bf203c3cae2511ca89e4aee6b898fc41
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-02 14:48:33 +0900

    style : SignUp Page 스타일링

--- client/src/pages/SignUp/index.tsx
+++ client/src/pages/SignUp/index.tsx
@@ -1,7 +1,12 @@
 import React,{useState} from 'react';
 import BackgroundLayout from '../../layouts/BackgroundLayout'
 import {
-  SignUpWrapper
+  SignUpWrapper,
+  Title,
+  InputPart,
+  ErrorResponse,
+  SignUpButton,
+  ButtonWrapper
 } from './style';
 
 function SignUp() {
@@ -9,27 +14,27 @@ function SignUp() {
   return (
     <BackgroundLayout>
       <SignUpWrapper>
-        <h1>계정 만들기</h1>
-        <div>
+        <Title>계정 만들기</Title>
+        <InputPart>
           <label htmlFor="user_id">아이디</label>
           <input id="user_id" type="text" />
-          <p>이미 사용중인 아이디 입니다.</p>
-        </div>
-        <div>
+          <ErrorResponse>이미 사용중인 아이디 입니다.</ErrorResponse>
+        </InputPart>
+        <InputPart>
           <label htmlFor="user_name">사용자명</label>
           <input id="user_name" type="text" />
-        </div>
-        <div>
+        </InputPart>
+        <InputPart>
         <label htmlFor="user_password">비밀번호</label>
           <input id="user_password" type="password" />
-          <p>비밀먼호는 특수문자 포함 6자 이상으로 해주세요.</p>
-        </div>
-        <div>
-          <button>
+          <ErrorResponse>비밀먼호는 특수문자 포함 6자 이상으로 해주세요.</ErrorResponse>
+        </InputPart>
+        <ButtonWrapper>
+          <SignUpButton>
               <p>가입하기</p>
-          </button>
+          </SignUpButton>
           <div>이미 계정이 있으신가요?</div>
-        </div>
+        </ButtonWrapper>
       </SignUpWrapper>
     </BackgroundLayout>
   );
