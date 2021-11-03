c19f3a7608a0dbf82abff1561a2caa54516afbf3
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-03 21:57:52 +0900

    Feat : 선택된 채널 정보를 저장하기 위한 selectedChannelSlice

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import selectedChannelReducer from './selectedChannel/slice';
 import selectedGroupReducer from './selectedGroup/slice';
 
-const store = configureStore({ reducer: { selectedGroup: selectedGroupReducer } });
+const store = configureStore({
+  reducer: { selectedGroup: selectedGroupReducer, selectedChannel: selectedChannelReducer },
+});
 
 export default store;
 
