6832b516ba395263dfc26f8edffe66f5315fdefe
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-03 18:40:40 +0900

    refactor : chat 메시지 API 리팩토링

--- server/src/controller/channel.controller.ts
+++ server/src/controller/channel.controller.ts
@@ -4,15 +4,22 @@ import { textChannelRepository, userRepository, textRepository } from '../db';
 
 import { Text } from '../entity/text.entity';
 
+export const createTextMSG = {
+  userNotFound: '존재하지 않는 사용자 입니다.',
+  emptyChat: '채팅을 입력해 주세요.',
+  success: '메시지 전송 성공!',
+};
+
 const createText = async (req: Request, res: Response, next: NextFunction) => {
-  const { content, textChannelId } = req.body;
+  const { content } = req.body;
+  const { textChannelId } = req.params;
   try {
     const { userID } = req.session;
     const user = await userRepository.findOne({ where: { id: userID } });
     const textChannel = await textChannelRepository.findOne({ where: { id: textChannelId } });
 
-    if (!user) return res.status(400).send('존재하지 않는 사용자 입니다.');
-    if (!content.trim()) return res.status(400).send('채팅을 입력해주세요.');
+    if (!user) return res.status(400).send(createTextMSG.userNotFound);
+    if (!content.trim()) return res.status(400).send(createTextMSG.emptyChat);
 
     const newText = new Text();
     newText.content = content;
@@ -21,7 +28,7 @@ const createText = async (req: Request, res: Response, next: NextFunction) => {
 
     await textRepository.save(newText);
 
-    return res.status(200).json(newText);
+    return res.status(200).send(createTextMSG.success);
   } catch (error) {
     next(error);
   }
