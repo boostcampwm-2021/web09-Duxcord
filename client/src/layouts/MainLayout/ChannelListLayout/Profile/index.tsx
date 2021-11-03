a470704554ed2808044d260b9a7a95b450e623a7
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-03 11:14:54 +0900

    refactor: ChannelListLayout에서 Profile 분리

---
+++ client/src/layouts/MainLayout/ChannelListLayout/Profile/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ProfileWrapper } from './style';
+
+interface Props {
+  device: {
+    mic: boolean,
+    speaker: boolean,
+    cam: boolean,
+  },
+  onToggleDevice: (device: "mic" | "speaker" | "cam") => void;
+}
+
+function Profile({ device, onToggleDevice }: Props) {
+
+  return (
+    <ProfileWrapper>
+        <div>
+          <div></div>
+          <p>부덕이</p>
+        </div>
+        <div>
+          <img onClick={() => onToggleDevice('mic')} src={"icons/mic" + (device.mic ? 'On' : 'Off') + ".png"} alt="micToggle" />
+          <img onClick={() => onToggleDevice('speaker')} src={"icons/speaker" + (device.speaker ? 'On' : 'Off') + ".png"} alt="speakerToggle" />
+          <img onClick={() => onToggleDevice('cam')} src={"icons/cam" + (device.cam ? 'On' : 'Off') + ".png"} alt="comToggle" />
+        </div>
+      </ProfileWrapper>
+  );
+}
+
+export default Profile;
