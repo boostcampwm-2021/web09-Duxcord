c6f18aa5aa474ef5138f1c6d4e4694d6945050e0
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-04 00:25:05 +0900

    Fix : Main 페이지에서 쓰이지 않는 Props 제거 및 Chat 조건부 렌더링

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ChannelHeader from '../../components/ChannelHeader';
 import Chat from '../../components/Chat';
 import SideBar from '../../components/SideBar';
+import { RootState } from '../../redux/store';
 import { Layout, MainWrapper } from './style';
 
-interface Props {
-  children: JSX.Element[] | JSX.Element;
-}
-
-function Main({ children }: Props) {
+function Main() {
+  const selectedChannel = useSelector((state: RootState) => state.selectedChannel);
   return (
     <Layout>
       <SideBar />
       <MainWrapper>
         <ChannelHeader />
-        {children}
-        <Chat chats={[]}/>
+        {selectedChannel.type === 'text' && <Chat />}
       </MainWrapper>
     </Layout>
   );
