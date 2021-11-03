e1b9dce6f96897b5817e052f34df5104d73823cb
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-03 19:20:19 +0900

    Refactor : 사이드바의 Profile 컴포넌트가 Prop로 상태를 전달받지 않고 직접 관리하도록 수정

--- client/src/components/SideBar/Profile/index.tsx
+++ client/src/components/SideBar/Profile/index.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProfileWrapper } from './style';
 
-interface Props {
-  device: {
-    mic: boolean,
-    speaker: boolean,
-    cam: boolean,
-  },
-  onToggleDevice: (device: "mic" | "speaker" | "cam") => void;
-}
+function Profile() {
+  const [device, setDevice] = useState({
+    mic: true,
+    speaker: true,
+    cam: true,
+  });
 
-function Profile({ device, onToggleDevice }: Props) {
+  const onToggleDevice = (target: 'mic' | 'speaker' | 'cam') => {
+    setDevice({
+      ...device,
+      [target]: !device[target],
+    });
+  };
 
   return (
     <ProfileWrapper>
-        <div>
-          <div></div>
-          <p>부덕이</p>
-        </div>
-        <div>
-          <img onClick={() => onToggleDevice('mic')} src={"icons/mic" + (device.mic ? 'On' : 'Off') + ".png"} alt="micToggle" />
-          <img onClick={() => onToggleDevice('speaker')} src={"icons/speaker" + (device.speaker ? 'On' : 'Off') + ".png"} alt="speakerToggle" />
-          <img onClick={() => onToggleDevice('cam')} src={"icons/cam" + (device.cam ? 'On' : 'Off') + ".png"} alt="camToggle" />
-        </div>
-      </ProfileWrapper>
+      <div>
+        <div></div>
+        <p>부덕이</p>
+      </div>
+      <div>
+        <img
+          onClick={() => onToggleDevice('mic')}
+          src={'icons/mic' + (device.mic ? 'On' : 'Off') + '.png'}
+          alt="micToggle"
+        />
+        <img
+          onClick={() => onToggleDevice('speaker')}
+          src={'icons/speaker' + (device.speaker ? 'On' : 'Off') + '.png'}
+          alt="speakerToggle"
+        />
+        <img
+          onClick={() => onToggleDevice('cam')}
+          src={'icons/cam' + (device.cam ? 'On' : 'Off') + '.png'}
+          alt="camToggle"
+        />
+      </div>
+    </ProfileWrapper>
   );
 }
 
