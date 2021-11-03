a716a35f6e16ca438e42628330e342f783b0e6d3
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-03 11:33:25 +0900

    test : SignIn.test.tsx에 renderSignIn 함수 추가

--- client/src/pages/SignIn/index.test.tsx
+++ client/src/pages/SignIn/index.test.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import SignIn from './index';
 
 describe('SignIn은', () => {
+  const renderSignIn = () => {
+    return render(
+      <MemoryRouter>
+        <SignIn />
+      </MemoryRouter>,
+    );
+  }
+
   context('정상적으로 그려질 때', () => {
     it('"돌아오신 것을 환영해요!"라는 문자를 보여준다.', () => {
-      const { queryByText } = render(<SignIn />);
+      const { queryByText } = renderSignIn();
       expect(queryByText('돌아오신 것을 환영해요!')).toBeInTheDocument();
     })
   })
 
   it('ID를 입력하면 Input이 바뀐다.',() => {
-    const { container } = render(<SignIn />);
+    const { container } = renderSignIn();
     
     fireEvent.input(container.querySelectorAll('input')[0], {
       target: {
@@ -22,7 +31,7 @@ describe('SignIn은', () => {
   })
 
   it('password를 입력하면 Input이 바뀐다.',() => {
-    const { container } = render(<SignIn />);
+    const { container } = renderSignIn();
     
     fireEvent.input(container.querySelectorAll('input')[1], {
       target: {
