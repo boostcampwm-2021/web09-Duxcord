221d1fe3a3e14e9c9917e1c4c101f2f43f387305
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-03 17:58:57 +0900

    feat : textRepository 생성

---
+++ server/src/controller/channel.controller.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { textChannelRepository, userRepository } from '../db';
+
+import { Text } from '../entity/text.entity';
+
+const createText = async (req: Request, res: Response, next: NextFunction) => {
+  const { content, textChannelId } = req.body;
+  try {
+    const { userID } = req.session;
+    const user = await userRepository.findOne({ where: { id: userID } });
+    const textChannel = await textChannelRepository.findOne({ where: { id: textChannelId } });
+
+    if (!user) return res.status(400).send('존재하지 않는 사용자 입니다.');
+    if (!content.trim()) return res.status(400).send('채팅을 입력해주세요.');
+
+    const newText = new Text();
+    newText.content = content;
+    newText.user = user;
+    newText.textChannel = textChannel;
+
+    await textChannelRepository.save(newText);
+
+    return res.status(200).json(newText);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default { createText };
