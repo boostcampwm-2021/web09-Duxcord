02b449c4090511d25d0c1e5f30fc139251c61c64
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-03 19:19:39 +0900

    Feat : 사이드바의 GroupSetting 컴포넌트가 현재 선택된 그룹 이름을 표시

--- client/src/components/SideBar/GroupSetting/index.tsx
+++ client/src/components/SideBar/GroupSetting/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useSelectedGroup } from '../../../hooks/useSelectedGroup';
 import { GroupSettingWrapper } from './style';
 
 function GroupSetting() {
+  const selectedGroup = useSelectedGroup();
 
   return (
     <GroupSettingWrapper>
-      <p>F4</p>
+      <p>{selectedGroup?.name}</p>
       <div>
         <img src="icons/inviteGroup.png" alt="inviteGroup" />
         <img src="icons/delete.png" alt="deleteGroup" />
