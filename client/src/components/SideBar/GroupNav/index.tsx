b7a0133972128fbcf13229d29c67a4b16b9e9de8
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-03 22:04:09 +0900

    Feat : 그룹 선택하면 채널 선택 초기화되게 변경

--- client/src/components/SideBar/GroupNav/index.tsx
+++ client/src/components/SideBar/GroupNav/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 import { useGroups } from '../../../hooks/useGroups';
+import { setSelectedChannel } from '../../../redux/selectedChannel/slice';
 import { setSelectedGroup } from '../../../redux/selectedGroup/slice';
 import { GroupListWrapper, GroupList, Group, GroupListDivider, AddGroupButton } from './style';
 
 function GroupNav() {
   const { groups } = useGroups();
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  const selectGroup = (group: any) => () => {
+    history.push(`/Main/group/${group.id}`);
+    dispatch(setSelectedChannel({ type: '', id: null, name: '' }));
+    dispatch(setSelectedGroup(group));
+  };
 
   return (
     <GroupListWrapper>
       <GroupList>
         {groups?.map((group: any) => (
-          <Group
-            key={group.id}
-            onClick={() => {
-              dispatch(setSelectedGroup(group));
-            }}
-          >
+          <Group key={group.id} onClick={selectGroup(group)}>
             {group.name}
           </Group>
         ))}
