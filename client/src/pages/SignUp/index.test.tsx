aea7efd39f941eb6fca8dc351edb8e05a20bafee
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-03 12:27:04 +0900

    test : SignUp.test.tsx에 renderSignUp 함수 추가

--- client/src/pages/SignUp/index.test.tsx
+++ client/src/pages/SignUp/index.test.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import SignUp from './index';
 
 describe('SignUp은', () => {
+  const renderSignUp = () => {
+    return render(
+      <MemoryRouter>
+        <SignUp />
+      </MemoryRouter>,
+    );
+  }
+
   context('정상적으로 그려질 때', () => {
     it('"계정 만들기"라는 문자를 보여준다.', () => {
-      const { queryByText } = render(<SignUp />);
+      const { queryByText } = renderSignUp();
       expect(queryByText('계정 만들기')).toBeInTheDocument();
     })
   })
