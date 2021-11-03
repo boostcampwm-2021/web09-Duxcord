41081b9c5faaa95af057d83286cb6bb8a5e13c55
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-03 13:53:19 +0900

    fix: camToggle 오타 수정

--- client/src/layouts/MainLayout/ChannelListLayout/Profile/index.tsx
+++ client/src/layouts/MainLayout/ChannelListLayout/Profile/index.tsx
@@ -21,7 +21,7 @@ function Profile({ device, onToggleDevice }: Props) {
         <div>
           <img onClick={() => onToggleDevice('mic')} src={"icons/mic" + (device.mic ? 'On' : 'Off') + ".png"} alt="micToggle" />
           <img onClick={() => onToggleDevice('speaker')} src={"icons/speaker" + (device.speaker ? 'On' : 'Off') + ".png"} alt="speakerToggle" />
-          <img onClick={() => onToggleDevice('cam')} src={"icons/cam" + (device.cam ? 'On' : 'Off') + ".png"} alt="comToggle" />
+          <img onClick={() => onToggleDevice('cam')} src={"icons/cam" + (device.cam ? 'On' : 'Off') + ".png"} alt="camToggle" />
         </div>
       </ProfileWrapper>
   );
