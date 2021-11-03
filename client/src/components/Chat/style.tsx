886868c15d339be36f22a553cd32b8e8605a6325
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-04 00:39:42 +0900

    Style : 채팅 메시지 리스트 커스텀 스크롤바 적용

--- client/src/components/Chat/style.tsx
+++ client/src/components/Chat/style.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import colors from '../../styles/colors';
 
 const ChatContainer = styled.div`
   flex: 1;
@@ -8,6 +9,20 @@ const Chats = styled.div`
   overflow-y: scroll;
   flex: 1;
   height: calc(100vh - 108px);
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${colors.Gray2};
+    border-radius: 10px;
+  }
+
+  &:hover::-webkit-scrollbar {
+    display: block;
+    width: 15px;
+  }
 `;
 
 export { ChatContainer, Chats };
