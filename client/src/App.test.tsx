739c2899417ed4adb48264ce56f5345db0d6ff01
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-03 11:02:12 +0900

    test : App.test.jsx에서 App을 그리는 전용 함수 생성

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 describe('App은', () => {
+
+  const renderApp = ({ path }) => {
+    return render(
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>,
+    );
+  }
+
   context('정상적으로 그려질 때', () => {
-    it('START라는 텍스트를 화면에 그린다.', () => {
-      const { container } = render(<App />);
+    it('전체를 감싸는 div(id=app)이 그려진다.', () => {
+      const { container } = renderApp({ path: '/' })
       expect(container.querySelector('.App')).toBeInTheDocument();
     })
   })
