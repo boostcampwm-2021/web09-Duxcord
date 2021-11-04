e9cbe70ee4edcd0e0ed3266fb77e5755dd9b3a8c
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-04 15:38:17 +0900

    feat: 사용자 정보 조회, 수정 api 연결

--- server/src/router/api/user.router.ts
+++ server/src/router/api/user.router.ts
@@ -5,6 +5,8 @@ export const userRouter = Router();
 
 userRouter.get('/', accessControl(), userController.getUserData);
 userRouter.get('/groups', accessControl({ signIn: true }), userController.getUserGroups);
+userRouter.get('/:id/profile', accessControl({ signIn: true }), userController.getOtherUserData);
 userRouter.post('/signup', accessControl({ signIn: false }), userController.signUp);
 userRouter.post('/signin', accessControl({ signIn: false }), userController.signIn);
 userRouter.post('/signout', accessControl(), userController.signOut);
+userRouter.patch('/profile', accessControl({ signIn: true }), userController.updateUserData);
