11227ed3e29099f7e554a56926d79270bf0515cc
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-04 15:17:53 +0900

    Feat : 그룹 선택 변경시 채널 Socket.io Room 나가기

--- client/src/components/SideBar/GroupNav/index.tsx
+++ client/src/components/SideBar/GroupNav/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
 import { useGroups } from '../../../hooks/useGroups';
+import { useSelectedChannel } from '../../../hooks/useSelectedChannel';
 import { setSelectedChannel } from '../../../redux/selectedChannel/slice';
 import { setSelectedGroup } from '../../../redux/selectedGroup/slice';
+import { socket } from '../../../util/socket';
 import { GroupListWrapper, GroupList, Group, GroupListDivider, AddGroupButton } from './style';
 
 function GroupNav() {
   const { groups } = useGroups();
+  const { id } = useSelectedChannel();
   const dispatch = useDispatch();
   const history = useHistory();
 
   const selectGroup = (group: any) => () => {
     history.push(`/Main/group/${group.id}`);
+    socket.emit('leaveChannel', id);
     dispatch(setSelectedChannel({ type: '', id: null, name: '' }));
     dispatch(setSelectedGroup(group));
   };
