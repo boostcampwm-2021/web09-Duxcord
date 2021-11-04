304ec450e0805f39b14397b89f4ab70e5661e51e
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-04 15:41:13 +0900

    feat : 회원가입 요청 함수 생성

--- client/src/util/api.ts
+++ client/src/util/api.ts
@@ -10,6 +10,17 @@ const tryLogin = async (loginID: string, password: string) => {
   return {status: response.status, responseText};
 }
 
+const trySignUp = async (loginID:string, username:string, password:string) => {
+  const response = await fetch(
+    'http://localhost:8000/api/user/signup', 
+    postFetchOptions({loginID, username, password})
+  )
+  const responseText = await response.text();
+
+  return {status: response.status, responseText};
+}
+
 export {
-  tryLogin
+  tryLogin,
+  trySignUp
 }
