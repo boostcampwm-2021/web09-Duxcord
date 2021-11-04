24a8cbf8dbc36ad2013da66a552b616f4f09059b
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-04 15:18:11 +0900

    Feat : 채팅 및 인피니티 스크롤

--- client/src/components/Chat/ChatInput/index.tsx
+++ client/src/components/Chat/ChatInput/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { FormEvent, useRef } from 'react';
+import { postChat } from '../../../api/postChat';
+import { useSelectedChannel } from '../../../hooks/useSelectedChannel';
 import { ButtonWrapper, Input, Wrapper } from './style';
 
-function ChatInput() {
+function ChatInput({ scrollToBottom }: { scrollToBottom: Function }) {
+  const { id } = useSelectedChannel();
+  const chatInputRef = useRef<HTMLInputElement>(null);
+
+  const onSubmitChat = async (e: FormEvent) => {
+    e.preventDefault();
+    if (chatInputRef.current === null) return;
+    const content = chatInputRef.current.value.trim();
+    if (content === '' || id === null) return;
+    postChat({ channelID: id, content: content });
+    chatInputRef.current.value = '';
+    scrollToBottom();
+  };
+
   return (
-    <Wrapper>
+    <Wrapper onSubmit={onSubmitChat}>
       <ButtonWrapper>
         <img src="/icons/btn-text-add-file.svg" alt="add file button" />
       </ButtonWrapper>
-      <Input placeholder="Message to channel" />
+      <Input ref={chatInputRef} placeholder="Message to channel" />
     </Wrapper>
   );
 }
--- client/src/components/Chat/index.tsx
+++ client/src/components/Chat/index.tsx
@@ -1,18 +1,78 @@
-import React from "react";
-import ChatItem from "./ChatItem";
-import { ChatContainer, Chats } from "./style";
-import { ChatData } from "../../types/chats";
-import ChatInput from "./ChatInput";
+import React, { useCallback, useEffect, useRef } from 'react';
+import ChatItem from './ChatItem';
+import { ChatContainer, Chats } from './style';
+import { ChatData } from '../../types/chats';
+import ChatInput from './ChatInput';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import useSWRInfinite from 'swr/infinite';
+import { socket } from '../../util/socket';
+import { getFetcher } from '../../util/fetcher';
 
-function Chat({ chats }: { chats: ChatData[] }) {
+const getKey = (channelID: number | null) => (index: number, prevData: any) => {
+  if (prevData && !prevData.length) return null;
+  return `/api/channel/${channelID}?page=${index + 1}`;
+};
+
+const PAGE_SIZE = 20;
+const THRESHOLD = 300;
+
+function Chat() {
+  const selectedChannel = useSelector((state: RootState) => state.selectedChannel);
+  const { data: chats, mutate, setSize } = useSWRInfinite(getKey(selectedChannel.id), getFetcher);
+  const isEmpty = chats?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chats && chats[chats.length - 1]?.length < PAGE_SIZE);
+  const chatListRef = useRef<HTMLDivElement>(null);
+
+  const onScroll = useCallback(() => {
+    if (chatListRef?.current?.scrollTop === 0 && !isReachingEnd) setSize((size) => size + 1);
+  }, [isReachingEnd, setSize]);
+
+  useEffect(() => {
+    const chatListEl = chatListRef.current;
+    chatListEl?.addEventListener('scroll', onScroll);
+
+    return () => chatListEl?.removeEventListener('scroll', onScroll);
+  }, [onScroll]);
+
+  const scrollToBottom = () => {
+    if (chatListRef.current === null) return;
+    chatListRef.current.scrollTop = chatListRef.current?.scrollHeight;
+  };
+
+  useEffect(scrollToBottom, []);
+
+  const onChat = useCallback(
+    async (chat: ChatData) => {
+      await mutate((chats) => {
+        chats?.unshift(chat);
+        return chats;
+      });
+      const { scrollTop, clientHeight, scrollHeight } = chatListRef.current as HTMLDivElement;
+      if (scrollHeight - (scrollTop + clientHeight) < THRESHOLD) scrollToBottom();
+    },
+    [chats, mutate]
+  );
+
+  useEffect(() => {
+    socket.on('chat', onChat);
+
+    return () => {
+      socket.off('chat', onChat);
+    };
+  }, [mutate, onChat]);
+
   return (
     <ChatContainer>
-      <Chats>
-        {chats.map((chat) => (
-          <ChatItem key={chat.id} chatData={chat} />
-        ))}
+      <Chats ref={chatListRef}>
+        {chats
+          ?.flat()
+          .reverse()
+          .map((chat) => (
+            <ChatItem key={chat.id} chatData={chat} />
+          ))}
       </Chats>
-      <ChatInput />
+      <ChatInput scrollToBottom={scrollToBottom} />
     </ChatContainer>
   );
 }
