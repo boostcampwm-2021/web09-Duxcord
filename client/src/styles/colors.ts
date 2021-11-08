b2298a67a6ee0f8c03ae95bca5409730c6438e36
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-08 14:54:10 +0900

    refactor: styles Colors enum으로 변경

--- client/src/styles/colors.ts
+++ client/src/styles/colors.ts
@@ -1,21 +1,17 @@
-interface Colors {
-  [colorName: string]: string;
+enum Colors {
+  Blue = '#0055FB',
+  LightRed = '#EE6762',
+  DarkRed = '#AB342E',
+  Yellow = 'F8B301',
+  Green = '#7CC092',
+  Black = '#222222',
+  Gray1 = '#4F5660',
+  Gray2 = '#D3D7DB',
+  Gray3 = '#EBEDEF',
+  Gray4 = '#E3E5E8',
+  Gray5 = '#F8F8F8',
+  Gray6 = '#F2F3F5',
+  White = '#FFFFFF',
 }
 
-const colors: Colors = {
-  Blue: '#0055FB',
-  LightRed: '#EE6762',
-  DarkRed: '#AB342E',
-  Yellow: 'F8B301',
-  Green: '#7CC092',
-  Black: '#222222',
-  Gray1: '#4F5660',
-  Gray2: '#D3D7DB',
-  Gray3: '#EBEDEF',
-  Gray4: '#E3E5E8',
-  Gray5: '#F8F8F8',
-  Gray6: '#F2F3F5',
-  White: '#FFFFFF',
-}
-
-export default colors;
+export default Colors;
