ba3bedf98209822d97936a34d3ff81b4c2d8b41d
seojin kim <001106ksj@gmail.com>
2021-11-08 17:20:05 +0900

    feat: 좋아요 달기 / 좋아요 지우기 구현

--- server/src/controller/chat.controller.ts
+++ server/src/controller/chat.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-import { userRepository, chatRepository, threadRepository } from '../db';
+import { userRepository, chatRepository, threadRepository, reactionRepository } from '../db';
 
 import { Thread } from '../entity/thread.entity';
 
@@ -11,13 +11,48 @@ export const createChatMSG = {
   success: '스레드 전송 성공!',
 };
 
+const handleReactionMSG = {
+  userNotFound: '존재하지 않는 사용자 입니다.',
+  chatNotFound: '존재하지 않는 텍스트 입니다.',
+  deleteReactionSuccess: '좋아요 지우기 성공!',
+  addReactionSuccess: '좋아요 만들기 성공!',
+};
+
+const handleReaction = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { chatID } = req.params;
+    const { userID } = req.session;
+
+    const user = await userRepository.findOne({ where: { id: userID } });
+    const chat = await chatRepository.findOne({ where: { id: chatID } });
+    if (!user) return res.status(400).send(handleReactionMSG.userNotFound);
+    if (!chat) return res.status(400).send(handleReactionMSG.chatNotFound);
+
+    const reaction = await reactionRepository.findOne({ where: { user: user, chat: chat } });
+
+    if (!reaction) {
+      await reactionRepository.insert({ user: user, chat: chat });
+      chat.reactionsCount += 1;
+      await chatRepository.save(chat);
+      res.status(200).send(handleReactionMSG.addReactionSuccess);
+    } else {
+      await reactionRepository.remove(reaction);
+      chat.reactionsCount -= 1;
+      await chatRepository.save(chat);
+      res.status(200).send(handleReactionMSG.deleteReactionSuccess);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
 const createThread = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { content } = req.body;
-    const { id } = req.params;
+    const { chatID } = req.params;
     const { userID } = req.session;
     const user = await userRepository.findOne({ where: { id: userID } });
-    const chat = await chatRepository.findOne({ where: { id: id } });
+    const chat = await chatRepository.findOne({ where: { id: chatID } });
 
     if (!user) return res.status(400).send(createChatMSG.userNotFound);
     if (!chat) return res.status(400).send(createChatMSG.chatNotFound);
@@ -36,4 +71,4 @@ const createThread = async (req: Request, res: Response, next: NextFunction) => 
   }
 };
 
-export default { createThread };
+export default { createThread, handleReaction };
