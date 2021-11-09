e58fe61a7221170c0e50efdc129b813648eb5510
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-09 10:43:32 +0900

    feat : 소켓에 현재 접속자 접속을 관리하도록 수정

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -3,12 +3,51 @@ import { Server } from 'socket.io';
 export let io: Server;
 
 export async function socketInit(httpServer) {
+  const userConnectionInfo = {};
   io = new Server(httpServer);
-
   io.on('connection', (socket) => {
+    //
+    socket.on('GroupID', (groupID) => {
+      console.log(userConnectionInfo, groupID);
+      socket.emit('GroupUserConnection', userConnectionInfo[groupID]);
+    });
+
+    socket.on('logIn', (groups, user) => {
+      if (!user || !groups) return;
+
+      groups?.forEach(({ code }) => {
+        socket.join(`${code}`);
+        io.to(`${code}`).emit('userEnter', user, code);
+        if (userConnectionInfo[code]) {
+          if (!userConnectionInfo[code].map((v) => v.loginID).includes(user.loginID)) {
+            userConnectionInfo[code].push(user);
+          }
+        } else {
+          userConnectionInfo[code] = [user];
+        }
+      });
+      const myFriendsConnectionInfo = {};
+      groups
+        .map((v) => v.code)
+        .forEach((code) => {
+          myFriendsConnectionInfo[code] = userConnectionInfo[code];
+        });
+      io.to(socket.id).emit('userConnectionInfo', myFriendsConnectionInfo);
+      socket.on('disconnect', () => {
+        Object.keys(userConnectionInfo).forEach((v) => {
+          userConnectionInfo[v] = userConnectionInfo[v].filter((a) => a.loginID !== user.loginID);
+        });
+        console.log(userConnectionInfo);
+        groups?.forEach(({ code }) => {
+          io.to(`${code}`).emit('userExit', user, code);
+        });
+      });
+    });
+
     socket.on('joinChannel', (channelID) => {
       socket.join(`${channelID}`);
     });
+
     socket.on('leaveChannel', (channelID) => {
       socket.leave(`${channelID}`);
     });
