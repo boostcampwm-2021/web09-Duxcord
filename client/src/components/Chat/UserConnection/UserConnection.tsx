49f27994ad4e7aff42626af9f17f30a43c30b4c0
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-09 16:15:03 +0900

    feat : 현재 접속인원 보여주는 컴포넌트 스타일링

--- client/src/components/Chat/UserConnection/UserConnection.tsx
+++ client/src/components/Chat/UserConnection/UserConnection.tsx
@@ -4,35 +4,42 @@ import { useGroupConnection } from '../../../hooks/useGroupConnection';
 import { useSelectedGroup } from '../../../hooks/useSelectedGroup';
 import { getFetcher } from '../../../util/fetcher';
 import { socket } from '../../../util/socket';
-import { UserConnectionWrapper, Text } from './style';
+import { UserConnectionWrapper, Text, UserImage, UserTile } from './style';
 
 function UserConnection() {
   const selectedGroup = useSelectedGroup();
   const groupConnection = useGroupConnection();
   const { data = [] } = useSWR(`/api/group/${selectedGroup?.id}/members`, getFetcher);
+
   useEffect(() => {
     socket.emit('GroupID', selectedGroup?.code);
-  }, []);
+  }, [selectedGroup?.code]);
+
   return (
     <UserConnectionWrapper>
       <Text>Online</Text>
       {groupConnection.map((oneUser: any, i: number) => (
-        <div key={i}>
-          <div>{oneUser.thumbnail}</div>
-          <div>{oneUser.loginID}</div>
+        <UserTile key={i}>
+          <div>
+            <UserImage src={'/images/default_profile.png'} alt="user profile" />
+            <div className="on-line"></div>
+          </div>
           <div>{oneUser.username}</div>
-        </div>
+        </UserTile>
       ))}
       <Text>Offline</Text>
       {data
         .filter(
-          ({ user }: any) => !groupConnection.map((v: any) => v.username).includes(user.username),
+          ({ user }: any) => !groupConnection.map((v: any) => v.loginID).includes(user.loginID),
         )
         .map((offLineUser: any, i: number) => (
-          <div key={i}>
+          <UserTile key={i}>
+            <div>
+              <UserImage src={'/images/default_profile.png'} alt="user profile" />
+              <div className="off-line"></div>
+            </div>
             <div>{offLineUser.user.username}</div>
-            <div>{offLineUser.user.thumbnail}</div>
-          </div>
+          </UserTile>
         ))}
     </UserConnectionWrapper>
   );
