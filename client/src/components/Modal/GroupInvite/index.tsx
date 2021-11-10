45c24e2c3d8508e17159685483d79a84146a97e5
seojin kim <001106ksj@gmail.com>
2021-11-10 15:46:06 +0900

    fix: 복사 메시지를 객체로 분리

--- client/src/components/Modal/GroupInvite/index.tsx
+++ client/src/components/Modal/GroupInvite/index.tsx
@@ -5,21 +5,28 @@ import Colors from '../../../styles/Colors';
 import { ModalController } from '../../../types/modal';
 import { CodeWrapper } from './style';
 
+const pasteCodeMessage = {
+  IDLE: '',
+  SUCCESS: '복사가 완료되었습니다.',
+  FAIL: '복사에 실패했습니다. 다시 시도해주세요',
+};
+
 function GroupInviteModal({ controller: { hide, show } }: { controller: ModalController }) {
   const selectedGroup = useSelectedGroup();
-  const [copied, setCopied] = useState('');
+  const [copied, setCopied] = useState(pasteCodeMessage.IDLE);
 
   const pasteGroupCode = async () => {
     try {
       await window.navigator.clipboard.writeText(selectedGroup.code);
-      setCopied('복사가 완료되었습니다.');
+      setCopied(pasteCodeMessage.SUCCESS);
     } catch (error) {
       console.error(error);
+      setCopied(pasteCodeMessage.FAIL);
     }
   };
 
   const finishModal = () => {
-    setCopied('');
+    setCopied(pasteCodeMessage.IDLE);
     hide();
   };
 
