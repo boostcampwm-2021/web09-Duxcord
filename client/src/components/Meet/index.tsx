1131bbcff8f5a04fc4b55aba49b39274a17e3fed
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-10 15:11:26 +0900

    style: Meet 컴포넌트 레이아웃 적용

--- client/src/components/Meet/index.tsx
+++ client/src/components/Meet/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import MeetVideo from './MeetVideo';
 import MeetButton from './MeetButton';
+import { MeetWrapper } from './style';
 
 function Meet() {
   return (
-    <div>
+    <MeetWrapper>
       <MeetVideo />
       <MeetButton />
-    </div>
+    </MeetWrapper>
   );
 }
 
---
+++ client/src/components/Meet/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MeetWrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 50px);
+  padding: 10px;
+`;
+
+export { MeetWrapper };
