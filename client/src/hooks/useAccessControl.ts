a7e377d98d3ff2fca5a25be3af64063032624024
KIM CHAN HEE <superlipbalm@gmail.com>
2021-11-12 19:49:13 +0900

    Refactor : useAccessControl 개선 isValidating을 활용해 useUserdata(useSWR)의 fetch가 완료된 후 로그인 여부를 판별하도록 변경함

--- client/src/hooks/useAccessControl.ts
+++ client/src/hooks/useAccessControl.ts
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import { API_URL } from '../api/API_URL';
-import { STATUS_CODES } from '../api/STATUS_CODES';
 import { useUserdata } from './useUserdata';
 
 export const useAccessControl = ({
@@ -12,14 +10,11 @@ export const useAccessControl = ({
   redirectPath: string;
 }) => {
   const history = useHistory();
-  const { userdata } = useUserdata();
+  const { userdata, isValidating } = useUserdata();
 
   useEffect(() => {
-    if (userdata && signIn) return;
-    fetch(API_URL.user.getUserdata).then(({ status }) => {
-      const accessible =
-        (signIn && status === STATUS_CODES.OK) || (!signIn && status >= STATUS_CODES.BAD_REQUEST);
-      if (!accessible) history.push(redirectPath);
-    });
-  }, []);
+    if (isValidating) return;
+    const isAccessible = (signIn && userdata) || (!signIn && !userdata);
+    if (!isAccessible) history.push(redirectPath);
+  }, [isValidating, userdata, history, signIn, redirectPath]);
 };
