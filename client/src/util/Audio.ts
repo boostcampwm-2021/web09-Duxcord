e30cf0721d79bdc0264aae3722dfb9d1512813d4
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-15 17:13:16 +0900

    chore : 자신의 볼륨을 인식할 수 있는 audiometer생성

---
+++ client/src/util/Audio.ts
@@ -0,0 +1,33 @@
+const highlightMyVolume = (stream: MediaStream, myVideo: HTMLVideoElement) => {
+  const audioContext = new AudioContext();
+  const analyser = audioContext.createAnalyser();
+  const microphone = audioContext.createMediaStreamSource(stream);
+  const javascriptNode = audioContext?.createScriptProcessor(2048, 1, 1);
+
+  analyser.smoothingTimeConstant = 0.2;
+  analyser.fftSize = 1024;
+
+  microphone.connect(analyser);
+  analyser.connect(javascriptNode);
+  javascriptNode.connect(audioContext.destination);
+  javascriptNode.onaudioprocess = () => {
+    const array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
+    let values = 0;
+
+    const length = array.length;
+    for (var i = 0; i < length; i++) {
+      values += array[i];
+    }
+
+    const average = values / length;
+
+    if (Math.round(average) > 30) {
+      myVideo.classList.add('saying');
+    } else {
+      myVideo.classList.remove('saying');
+    }
+  };
+};
+
+export { highlightMyVolume };
