97d6995450a944343589bc17e10da4c008b909ed
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-15 19:01:57 +0900

    feat: GroupDeleteModal에 삭제하기 버튼을 누르면 그룹을 삭제하는 기능 구현

--- client/src/components/Modal/GroupDelete/index.tsx
+++ client/src/components/Modal/GroupDelete/index.tsx
@@ -1,16 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 import Modal from '..';
-import { useSelectedGroup } from '@hooks/index';
+import { deleteGroup } from '../../../api/deleteGroup';
+import { useSelectedGroup, useGroups } from '@hooks/index';
+import { setSelectedGroup } from '@redux/selectedGroup/slice';
+import { setSelectedChannel } from '@redux/selectedChannel/slice';
+import { setSelectedChat } from '@redux/selectedChat/slice';
 import Colors from '@styles/Colors';
 import { ModalController } from '@customTypes/modal';
 import { AlertWrapper } from './style';
+import { URL } from 'src/api/URL';
+import { socket } from '../../../util/socket';
+import GroupEvent from '@customTypes/socket/GroupEvent';
 
 function GroupDeleteModal({ controller: { hide, show } }: { controller: ModalController }) {
   const selectedGroup = useSelectedGroup();
+  const { groups, mutate: mutateGroups } = useGroups();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  const deleteGroup = () => {
+  const deleteCurrentGroup = async () => {
     try {
-      console.log('click');
+      const response = await deleteGroup({ groupID: selectedGroup.id });
+      switch (response.status) {
+        case 200:
+          mutateGroups(
+            groups.filter((group: any) => group.id !== selectedGroup.id),
+            false,
+          );
+          socket.emit(GroupEvent.groupDelete, selectedGroup.code);
+          dispatch(setSelectedGroup(null));
+          dispatch(
+            setSelectedChannel({
+              type: '',
+              id: null,
+              name: '',
+            }),
+          );
+          dispatch(setSelectedChat(null));
+          finishModal();
+          history.push(URL.groupPage());
+          break;
+        case 400:
+          const responseText = await response.text();
+          console.error(responseText);
+          break;
+        default:
+          console.log('백엔드가 포기한 요청');
+      }
     } catch (error) {
       console.error(error);
     }
@@ -36,7 +74,7 @@ function GroupDeleteModal({ controller: { hide, show } }: { controller: ModalCon
         bottomRightButton: {
           text: '삭제하기',
           color: Colors.Red,
-          onClickHandler: deleteGroup,
+          onClickHandler: deleteCurrentGroup,
         },
       }}
       controller={{ hide: finishModal, show }}
