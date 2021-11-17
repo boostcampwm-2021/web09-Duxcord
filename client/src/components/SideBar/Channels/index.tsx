9ec3743429ad682469120e3decf3ff27a2dab747
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-17 17:28:10 +0900

    refactor : 미팅채널일 경우만 소켓으로 받아오도록 if문 생성

--- client/src/components/SideBar/Channels/index.tsx
+++ client/src/components/SideBar/Channels/index.tsx
@@ -32,10 +32,12 @@ function Channels({ channelType }: Props) {
   const { groupID } = getURLParams();
 
   useEffect(() => {
+    if (channelType === 'chatting') return;
+
     const meetingchannelList = selectedGroup?.meetingChannels?.map(
       (channel: { id: number }) => channel.id,
     );
-
+    // 현재 클릭된 그룹의 미팅채널별 참여인원들을 받아온다.
     socket.emit(MeetEvent.MeetingChannelList, selectedGroup.code, meetingchannelList);
   }, [selectedGroup]);
 
