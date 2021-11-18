bec01b0fa26c3c6446e62d5a9f91a49a1526da4e
superLipbalm <superLipbalm@gmail.com>
2021-11-18 11:56:43 +0900

    Feat : OtherVideo에 화면공유 표시 추가

--- client/src/components/Meet/MeetVideo/OtherVideo/index.tsx
+++ client/src/components/Meet/MeetVideo/OtherVideo/index.tsx
@@ -5,11 +5,17 @@ import { VideoItemWrapper, VideoItem } from '../style';
 
 function OtherVideo({ member }: { member: IMeetingUser }) {
   const camRef = useRef<HTMLVideoElement>(null);
+  const screenRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     if (!camRef.current || !member.stream) return;
     camRef.current.srcObject = member.stream;
   }, [member.stream]);
+
+  useEffect(() => {
+    if (!screenRef.current || !member.screen) return;
+    screenRef.current.srcObject = member.screen;
+  }, [member.screen]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -29,14 +35,22 @@ function OtherVideo({ member }: { member: IMeetingUser }) {
   }, [member]);
 
   return (
-    <VideoItemWrapper>
+    <>
+      <VideoItemWrapper>
         <VideoItem key={member.socketID} autoPlay playsInline ref={camRef} />
-      <p>{member?.username}</p>
-      {member.mic || <MicOffIcon />}
-      {member.cam || (
-        <img src={member?.thumbnail || '/images/default_profile.png'} alt="profile"></img>
+        <p>{member?.username}</p>
+        {member.mic || <MicOffIcon />}
+        {member.cam || (
+          <img src={member?.thumbnail || '/images/default_profile.png'} alt="profile"></img>
+        )}
+      </VideoItemWrapper>
+      {member.screen && (
+        <VideoItemWrapper>
+          <VideoItem key={member.socketID} autoPlay playsInline ref={screenRef} />
+          <p>{member?.username + '님의 화면'}</p>
+        </VideoItemWrapper>
       )}
-    </VideoItemWrapper>
+    </>
   );
 }
 export default OtherVideo;
