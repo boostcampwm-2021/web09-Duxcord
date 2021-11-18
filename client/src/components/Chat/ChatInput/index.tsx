7c2ce7563398856cd09308870944e67016b6951c
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-18 11:01:04 +0900

    feat: ChatInput에서 files를 추가하는 기능 구현, content, files 중 하나의 요소만 있으면 메시지를 보낼 수 있도록 수정

--- client/src/components/Chat/ChatInput/index.tsx
+++ client/src/components/Chat/ChatInput/index.tsx
@@ -1,8 +1,9 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
 import { postChat } from '../../../api/postChat';
 import { useSelectedChannel } from '@hooks/index';
 import { FileSelectIcon } from '../../common/Icons';
-import { ButtonWrapper, InputWrapper, Wrapper } from './style';
+import { FileInputWrapper, ChatInputWrapper, Wrapper } from './style';
+import { uploadFileToStorage } from 'src/utils/uploadFile';
 
 function ChatInput({ scrollToBottom }: { scrollToBottom: () => void }) {
   const { id } = useSelectedChannel();
@@ -10,23 +11,56 @@ function ChatInput({ scrollToBottom }: { scrollToBottom: () => void }) {
 
   const onSubmitChat = async (e: FormEvent) => {
     e.preventDefault();
-    if (chatInputRef.current === null) return;
-    const content = chatInputRef.current.value.trim();
-    if (content === '' || id === null) return;
-    postChat({ channelID: id, content: content });
-    chatInputRef.current.value = '';
+    if (chatInputRef.current === null && !fileURL.length) return;
+    const content = chatInputRef.current ? chatInputRef.current.value.trim() : '';
+    if ((content === '' && !fileURL.length) || id === null) return;
+    postChat({ channelID: id, content: content, files: fileURL });
+    if (chatInputRef.current) chatInputRef.current.value = '';
+    setFileURL([]);
     scrollToBottom();
+  };
+  const [fileURL, setFileURL] = useState<string[]>([]);
+
+  const fileInputRef = useRef<HTMLDivElement>(null);
+  const uploadFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const target = e.target as HTMLInputElement;
+      if (!target.files) return;
+      const file: File = (target.files as FileList)[0];
+      if (!file.type.match('image/jpeg|image/png')) return;
+      const uploadedFile = await uploadFileToStorage(file);
+      if (uploadedFile && fileInputRef && fileInputRef.current) {
+        setFileURL([...fileURL, uploadedFile]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <Wrapper onSubmit={onSubmitChat}>
-      <ButtonWrapper>
+      <FileInputWrapper ref={fileInputRef}>
         <FileSelectIcon />
-      </ButtonWrapper>
-      <InputWrapper>
+        <input
+          type="file"
+          id="chat_upload"
+          onChange={uploadFile}
+          style={{ width: 40, height: 40, opacity: 0 }}
+          accept="image/jpeg, image/png"
+        />
+      </FileInputWrapper>
+      <ChatInputWrapper>
         <input ref={chatInputRef} placeholder="Message to channel" />
-        <div></div>
-      </InputWrapper>
+        {fileURL.length !== 0 && (
+          <div>
+            {fileURL.map((url) => (
+              <div key={url}>
+                <img src={url} />
+              </div>
+            ))}
+          </div>
+        )}
+      </ChatInputWrapper>
     </Wrapper>
   );
 }
