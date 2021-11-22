231814b3604fa61489f227222f520aa46244582e
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-22 14:28:53 +0900

    feat : userDevice 상태관리 slice에 localStorage 참조하도록 재구현

--- client/src/redux/userDevice/slice.ts
+++ client/src/redux/userDevice/slice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { loadItem, saveItem } from 'src/utils/storage';
 
 const initState: { mic: boolean; speaker: boolean; cam: boolean } = {
   mic: true,
@@ -10,10 +11,17 @@ const { reducer: userDeviceReducer, actions } = createSlice({
   name: 'userDevice',
   initialState: initState,
   reducers: {
-    setUserDevice: (state, { payload: { mic, speaker, cam } }) => ({ mic, speaker, cam }),
+    setUserDevice: (state, { payload: { mic, speaker, cam } }) => {
+      saveItem('userDevice', { mic, speaker, cam });
+      return { mic, speaker, cam };
+    },
+    bringUserDevice: (state) => {
+      const userDevice = loadItem('userDevice');
+      return userDevice ? { ...JSON.parse(userDevice) } : { ...initState };
+    },
   },
 });
 
-export const { setUserDevice } = actions;
+export const { setUserDevice, bringUserDevice } = actions;
 
 export default userDeviceReducer;
