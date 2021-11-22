e77898f1ff832bb04bc0cc7efbc7f2c3c7fa7ed0
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-22 19:28:01 +0900

    feat : 이미지로딩이 끝난 후 새로운 채팅들의 높이를 계산하는 함수 생성

---
+++ client/src/utils/getChatsHeight.ts
@@ -0,0 +1,36 @@
+import { map, mapAsync, reduceAsync, filter } from './functionalProgramming';
+import { makeChatSection } from './makeChatSection';
+
+const getChatsHeight = async (chatListRef: React.RefObject<HTMLDivElement>, length: number) => {
+  const chatItems = chatListRef.current?.querySelectorAll('.ChatItem');
+  const target = filter(chatItems, length);
+  const DayPillHeights = Object.keys(makeChatSection(target)).length * 30;
+  let count = -1;
+  const result = await reduceAsync(
+    (a, b) => a + b,
+    0,
+    mapAsync(
+      () =>
+        new Promise((resolve) => {
+          count += 1;
+          return resolve(target[count].clientHeight);
+        }),
+      map(
+        (chatItem: HTMLElement) =>
+          Promise.all(
+            map(
+              (img: HTMLElement) =>
+                new Promise<void>((resolve) => {
+                  img.onload = () => resolve();
+                }),
+              chatItem.querySelectorAll('img'),
+            ),
+          ),
+        target,
+      ),
+    ),
+  );
+  return result + DayPillHeights;
+};
+
+export { getChatsHeight };
