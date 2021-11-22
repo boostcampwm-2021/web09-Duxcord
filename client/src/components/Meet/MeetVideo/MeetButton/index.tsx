dd2a7b13db24339d3f2cce46646812345b6cfc47
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-22 13:52:49 +0900

    feat: 통화 종료 버튼 기능 구현

--- client/src/components/Meet/MeetVideo/MeetButton/index.tsx
+++ client/src/components/Meet/MeetVideo/MeetButton/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 
+import { setSelectedChannel } from '@redux/selectedChannel/slice';
+import { useSelectedGroup } from '@hooks/index';
+import { URL } from 'src/api/URL';
 import { MeetingStopIcon, ScreenShareStartIcon } from '@components/common/Icons';
 import { MeetButtonWrapper } from './style';
 
-function MeetButton({ onClick }: { onClick: () => void }) {
+function MeetButton({ onScreenShareClick }: { onScreenShareClick: () => void }) {
+  const selectedGroup = useSelectedGroup();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const onMeetingStopClick = () => {
+    dispatch(
+      setSelectedChannel({
+        type: '',
+        id: null,
+        name: '',
+      }),
+    );
+    history.replace(URL.groupPage(selectedGroup.id));
+  };
+
   return (
     <MeetButtonWrapper>
-      <button onClick={onClick}>
+      <button onClick={onScreenShareClick}>
         <ScreenShareStartIcon />
       </button>
-      <button>
+      <button onClick={onMeetingStopClick}>
         <MeetingStopIcon />
       </button>
     </MeetButtonWrapper>
