d390f66c3e6b78eb60d4ce11d24cb2a42f640289
shinhyogeun <tls1gy2rms3@naver.com>
2021-11-23 15:13:22 +0900

    chore : 인자를 모두 받아야 실행되도록 curry함수 생성

--- client/src/utils/functionalProgramming.ts
+++ client/src/utils/functionalProgramming.ts
@@ -1,32 +1,41 @@
-const map = (f: (a: HTMLElement) => Promise<any>, iter: any) => {
+const curry =
+  (f: any) =>
+  (a: any, ..._: any) =>
+    _.length ? f(a, ..._) : (..._: any) => f(a, ..._);
+
+const map = curry((f: (a: HTMLElement) => Promise<any>, iter: any) => {
   const res = [];
   for (const a of iter) {
     res.push(f(a));
   }
   return res;
-};
+});
 
-const filter = (chatsList: NodeListOf<Element> | undefined, length: number) => {
+const filter = curry((chatsList: NodeListOf<Element> | undefined, length: number) => {
   const res: Element[] = [];
   chatsList?.forEach((v, i) => {
     if (i < length) res.push(v);
   });
   return res;
-};
+});
 
-const mapAsync = (f: (a: Promise<any>) => Promise<any>, iter: any) => {
+const mapAsync = curry((f: (a: Promise<any>) => Promise<any>, iter: any) => {
   const res = [];
   for (const a of iter) {
     res.push(a.then(f));
   }
   return res;
-};
+});
 
-const reduceAsync = async (f: (a: number, b: number) => number, acc: number, iter: any) => {
+const reduceAsync = curry(async (f: (a: number, b: number) => number, acc: any, iter: any) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = await iter.next().value;
+  }
   for await (const ab of iter) {
     acc = f(acc, ab);
   }
   return acc;
-};
+});
 
 export { map, mapAsync, filter, reduceAsync };
