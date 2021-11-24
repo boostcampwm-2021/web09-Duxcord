5048a04a523d57c27aa8a0147312df0c77aafeb6
seojin kim <001106ksj@gmail.com>
2021-11-25 04:29:19 +0900

    feat: 화면 캡쳐 인생네컷 기능 구현

--- client/src/components/Meet/MeetVideo/MeetButton/index.tsx
+++ client/src/components/Meet/MeetVideo/MeetButton/index.tsx
@@ -7,6 +7,7 @@ import { useSelectedGroup } from '@hooks/index';
 import { URL } from 'src/api/URL';
 import { CaptureIcon, MeetingStopIcon, ScreenShareStartIcon } from '@components/common/Icons';
 import { DarkRedButton, GrayButton, MeetButtonWrapper } from './style';
+import { capture } from 'src/utils/capture';
 
 function MeetButton({ onScreenShareClick }: { onScreenShareClick: () => void }) {
   const selectedGroup = useSelectedGroup();
@@ -24,7 +25,9 @@ function MeetButton({ onScreenShareClick }: { onScreenShareClick: () => void }) 
     history.replace(URL.groupPage(selectedGroup.id));
   };
 
-  const onMeetingCaptureClick = () => {};
+  const onMeetingCaptureClick = () => {
+    capture();
+  };
 
   return (
     <MeetButtonWrapper>
---
+++ client/src/utils/capture.ts
@@ -0,0 +1,74 @@
+interface HTMLVideoElementWithCaputreStream extends HTMLVideoElement {
+  captureStream(): MediaStream;
+}
+
+const CAPTURE = {
+  PADDING: 40,
+  PADDING_BOTTOM: 120,
+  CROP_WIDTH: 350,
+  CROP_HEIGHT: 280,
+  IMAGE_SIZE: 60,
+};
+
+const isWideImage = (imageBitmap: ImageBitmap) =>
+  CAPTURE.CROP_HEIGHT / CAPTURE.CROP_WIDTH > imageBitmap.height / imageBitmap.width;
+
+const capture = async () => {
+  const videos = document.querySelectorAll(
+    'video',
+  ) as unknown as Array<HTMLVideoElementWithCaputreStream>;
+  const canvas = document.createElement('canvas');
+
+  canvas.width = CAPTURE.CROP_WIDTH + 2 * CAPTURE.PADDING;
+  canvas.height = (CAPTURE.CROP_HEIGHT + CAPTURE.PADDING) * videos.length + CAPTURE.PADDING_BOTTOM;
+  const context = canvas.getContext('2d');
+
+  let index = 0;
+  for (const video of videos) {
+    const imageCapture = new ImageCapture(video.captureStream().getTracks()[1]);
+    const imageBitmap = await imageCapture.grabFrame();
+    const { width: bitmapWidth, height: bitmapHeight } = imageBitmap;
+    const isWide = isWideImage(imageBitmap);
+    context?.drawImage(
+      imageBitmap,
+      isWide ? (bitmapWidth - bitmapHeight * (CAPTURE.CROP_WIDTH / CAPTURE.CROP_HEIGHT)) / 2 : 0,
+      isWide ? 0 : (bitmapHeight - bitmapWidth * (CAPTURE.CROP_HEIGHT / CAPTURE.CROP_WIDTH)) / 2,
+      isWide ? bitmapHeight * (CAPTURE.CROP_WIDTH / CAPTURE.CROP_HEIGHT) : bitmapWidth,
+      isWide ? bitmapHeight : bitmapWidth * (CAPTURE.CROP_HEIGHT / CAPTURE.CROP_WIDTH),
+      CAPTURE.PADDING,
+      index * (CAPTURE.PADDING + CAPTURE.CROP_HEIGHT) + CAPTURE.PADDING,
+      CAPTURE.CROP_WIDTH,
+      CAPTURE.CROP_HEIGHT,
+    );
+    index++;
+  }
+
+  const logoImage = new Image();
+  logoImage.src = '/images/duxcord_logo.png';
+  console.log(logoImage);
+  logoImage.onload = () => {
+    context?.drawImage(
+      logoImage,
+      canvas.width - CAPTURE.IMAGE_SIZE - CAPTURE.PADDING,
+      canvas.height - CAPTURE.IMAGE_SIZE - CAPTURE.PADDING,
+      CAPTURE.IMAGE_SIZE,
+      CAPTURE.IMAGE_SIZE,
+    );
+
+    canvas.toBlob(
+      (blob) => {
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = URL.createObjectURL(blob);
+        a.download = `화면캡처${new Date()}`;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      },
+      'image/jpeg',
+      1,
+    );
+  };
+};
+
+export { capture };
