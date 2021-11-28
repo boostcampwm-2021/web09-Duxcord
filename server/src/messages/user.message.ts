10b2859b6a4a9574e4f2c713c0e99a12961553a7
superLipbalm <superLipbalm@gmail.com>
2021-11-29 00:19:32 +0900

    Fix : 비밀번호 규칙 영대소문자(대소문자 중 하나만 포함되어도 됨), 숫자, 특수문자 각 1자 이상 사용하도록 변경

--- server/src/messages/user.message.ts
+++ server/src/messages/user.message.ts
@@ -2,8 +2,7 @@ export const signUpMSG = {
   nullInput: '회원가입에 필요한 데이터를 모두 입력해주세요.',
   invalidLoginID: 'ID는 영소문자로 시작하는 6~15자의 영소문자 또는 숫자 여야 합니다.',
   invalidUsername: '유저 이름은 공백없이 1~15자여야 합니다.',
-  invalidPassword:
-    '비밀번호는 8자 이상 영대문자, 영소문자, 숫자, 특수문자를 최소 1개씩 포함하여야합니다.',
+  invalidPassword: '비밀번호는 8자 이상 영대소문자, 숫자, 특수문자를 최소 1개씩 포함하여야합니다.',
   usedID: '이미 사용중인 ID 입니다.',
   success: '회원가입에 성공했습니다.',
 };
