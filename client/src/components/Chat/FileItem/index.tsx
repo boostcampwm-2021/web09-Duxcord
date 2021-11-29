ec21abe955913281db013bf71ad9fb3562423666
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-29 15:55:45 +0900

    refactor: FileItem 컴포넌트 Props interface로 타입 선언 분리

--- client/src/components/Chat/FileItem/index.tsx
+++ client/src/components/Chat/FileItem/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { FileItemWrapper } from './style';
 
-function FileItem({ src, alt, itemType }: { src: string; alt: string; itemType: string }) {
+interface Props {
+  src: string;
+  alt: string;
+  itemType: string;
+}
+
+function FileItem({ src, alt, itemType }: Props) {
   return (
     <FileItemWrapper itemType={itemType}>
       <img src={src} alt={alt} />
