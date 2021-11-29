00492beb70acbac413ce27df406119c66b77ae26
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-29 15:54:33 +0900

    refactor: 가독성을 위해 백틱 사용

--- client/src/components/Chat/FileItem/style.ts
+++ client/src/components/Chat/FileItem/style.ts
@@ -7,11 +7,24 @@ interface IFileNameWrapper {
 const fileImgType = (type: string) => {
   switch (type) {
     case 'chat':
-      return 'max-width: 300px; height: 120px; margin-right: 10px; object-fit: cover;';
+      return `
+      max-width: 300px;
+      height: 120px;
+      margin-right: 10px;
+      object-fit: cover;
+      `;
     case 'chatInput':
-      return 'width: 120px; height: 120px; margin-right: 10px;';
+      return `
+      width: 120px;
+      height: 120px;
+      margin-right: 10px;
+      `;
     case 'thread':
-      return 'max-width: 200px; height: 120px; object-fit: cover;';
+      return `
+      max-width: 200px;
+      height: 120px;
+      object-fit: cover;
+      `;
   }
 };
 
