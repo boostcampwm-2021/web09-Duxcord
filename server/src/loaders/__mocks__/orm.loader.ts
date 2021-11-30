091404a127e69e3deb49aa49438ed0483d2f36ea
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-30 22:45:55 +0900

    test: group.controller 테스트를 위한 orm.loader mock 추가

--- server/src/loaders/__mocks__/orm.loader.ts
+++ server/src/loaders/__mocks__/orm.loader.ts
@@ -9,7 +9,9 @@ export const userRepository = {
 };
 
 export const chattingChannelRepository = {
-  findOne: jest.fn().mockResolvedValue('channel'),
+  findOne: jest.fn().mockResolvedValue('chattingChannel'),
+  save: jest.fn().mockResolvedValue('save'),
+  remove: jest.fn().mockResolvedValue('remove'),
 };
 
 export const fileRepository = {
@@ -24,5 +26,24 @@ export const reactionRepository = {
 
 export const threadRepository = {
   save: jest.fn().mockResolvedValue('save'),
-  findThreadsByChatID: jest.fn(),
+  findThreadsByChatID: jest.fn((value) => Number(value)),
+};
+
+export const groupRepository = {
+  findOne: jest.fn(),
+  save: jest.fn().mockResolvedValue('save'),
+  findByIDWithLeaderID: jest.fn(),
+  remove: jest.fn().mockResolvedValue('remove'),
+};
+
+export const groupMemberRepository = {
+  insert: jest.fn().mockResolvedValue('groupMemberInsert'),
+  findUsersByGroupID: jest.fn((value) => value),
+  checkUserInGroup: jest.fn(),
+};
+
+export const meetingChannelRepository = {
+  save: jest.fn().mockResolvedValue('save'),
+  findOne: jest.fn().mockResolvedValue('meetingChannel'),
+  remove: jest.fn().mockResolvedValue('remove'),
 };
