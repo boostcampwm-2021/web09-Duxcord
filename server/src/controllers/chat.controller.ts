3180a88018f34de8016429eee0a69bcea08d8149
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-11-30 20:29:44 +0900

    fix: 테스트를 위해 handleReactionMSG export
    Co-authored-by: shinhyogeun <shinhyogeun@users.noreply.github.com>

--- server/src/controllers/chat.controller.ts
+++ server/src/controllers/chat.controller.ts
@@ -17,7 +17,7 @@ export const createChatMSG = {
   success: '스레드 전송 성공!',
 };
 
-const handleReactionMSG = {
+export const handleReactionMSG = {
   userNotFound: '존재하지 않는 사용자 입니다.',
   chatNotFound: '존재하지 않는 텍스트 입니다.',
   deleteReactionSuccess: '좋아요 지우기 성공!',
