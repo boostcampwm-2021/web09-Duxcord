09e74b8ed3e99f4dba1ca53962b1eefc177d40d4
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-12-01 23:22:15 +0900

    fix: empty arrow function 오류 처리를 위한 broadcast mock 수정

--- server/src/utils/__mocks__/index.ts
+++ server/src/utils/__mocks__/index.ts
@@ -1,6 +1,6 @@
 export const broadcast = {
-  newChat: jest.fn((chat) => {}),
-  reactionInfo: jest.fn((chat) => {}),
-  threadInfo: jest.fn((chat) => {}),
-  newThread: jest.fn((chat) => {}),
+  newChat: jest.fn((chat) => chat),
+  reactionInfo: jest.fn((chat) => chat),
+  threadInfo: jest.fn((chat) => chat),
+  newThread: jest.fn((chat) => chat),
 };
