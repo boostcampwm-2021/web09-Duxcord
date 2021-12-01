bf2b0877d8eaa2a1e81db15715aaee72ce8476b0
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-12-01 17:58:34 +0900

    test: chat validator 테스트 코드 작성

--- server/src/utils/validators/chat.validator.test.ts
+++ server/src/utils/validators/chat.validator.test.ts
@@ -1,28 +1,15 @@
 import { Request, Response } from 'express';
-jest.mock('../../loaders/orm.loader');
-import { userRepository, chatRepository } from '../../loaders/orm.loader';
-import { createChatMSG, handleReactionMSG } from '../../messages';
 
-import { createThreadValidator, getThreadValidator, reactionValidator } from './index';
+jest.mock('../../loaders/orm.loader');
+
+import { userRepository, chatRepository } from '../../loaders/orm.loader';
+import { CREATE_CHAT_MSG, HANDLE_REACTION_MSG } from '../../messages';
+
+import { chatValidator } from './index';
 
 describe('chat.validator', () => {
-  const mockResponse = (result: string): Response => {
-    const res =
-      result === 'resolve'
-        ? ({
-            status: jest.fn((code) => res),
-            json: jest.fn((value) => value),
-            send: jest.fn((value) => value),
-          } as unknown)
-        : ({
-            status: jest.fn((code) => res),
-            json: jest.fn(() => {
-              throw Error;
-            }),
-            send: jest.fn(() => {
-              throw Error;
-            }),
-          } as unknown);
+  const mockResponse = (): Response => {
+    const res = {} as unknown;
     return res as Response;
   };
 
@@ -30,27 +17,19 @@ describe('chat.validator', () => {
     jest.clearAllMocks();
 
     userRepository.findOne = jest.fn().mockResolvedValue('user');
-
-    chatRepository.findOne = jest.fn().mockResolvedValue({
-      id: 1,
-      reactionsCount: 0,
-      chattingChannel: { id: 1 },
-      threadsCount: 0,
-      threadWriter: 'user',
-      threadLastTime: 0,
-    });
+    chatRepository.findOne = jest.fn().mockResolvedValue('chat');
   });
 
   describe('reactionValidator', () => {
-    const mockRequest = (value: boolean): Request => {
+    const mockRequest = (): Request => {
       const req = {
-        isAuthenticated: jest.fn(() => value),
         session: {
           userID: 'banana',
         },
         params: {
           chatID: 1,
         },
+        body: {},
       } as unknown;
       return req as Request;
     };
@@ -59,14 +38,13 @@ describe('chat.validator', () => {
       it('userNotFound 메시지를 반환한다', async () => {
         userRepository.findOne = jest.fn().mockResolvedValue(undefined);
 
-        const req = mockRequest(true);
-        const res = mockResponse('resolve');
+        const req = mockRequest();
+        const res = mockResponse();
         const next = jest.fn();
 
-        await reactionValidator(req, res, next);
+        await chatValidator.reactionValidator(req, res, next);
 
-        expect(res.status).toBeCalledWith(400);
-        expect(res.send).toBeCalledWith(handleReactionMSG.userNotFound);
+        expect(next).toBeCalledWith({ message: HANDLE_REACTION_MSG.USER_NOT_FOUND, status: 400 });
       });
     });
 
@@ -74,33 +52,32 @@ describe('chat.validator', () => {
       it('chatNotFound 메시지를 반환한다', async () => {
         chatRepository.findOne = jest.fn().mockResolvedValue(undefined);
 
-        const req = mockRequest(true);
-        const res = mockResponse('resolve');
+        const req = mockRequest();
+        const res = mockResponse();
         const next = jest.fn();
 
-        await reactionValidator(req, res, next);
+        await chatValidator.reactionValidator(req, res, next);
 
-        expect(res.status).toBeCalledWith(400);
-        expect(res.send).toBeCalledWith(handleReactionMSG.chatNotFound);
+        expect(next).toBeCalledWith({ message: HANDLE_REACTION_MSG.CHAT_NOT_FOUND, status: 400 });
       });
     });
 
-    context('error가 발생했을 때', () => {
+    context('error가 발생하지 않았을 때', () => {
       it('next가 실행된다', async () => {
-        const req = mockRequest(true);
-        const res = mockResponse('reject');
+        const req = mockRequest();
+        const res = mockResponse();
         const next = jest.fn();
 
-        await reactionValidator(req, res, next);
+        await chatValidator.reactionValidator(req, res, next);
 
-        expect(next).toBeCalled();
+        expect(next).toBeCalledWith();
       });
     });
   });
+
   describe('createThreadValidator', () => {
-    const mockRequest = (value: boolean, content: string): Request => {
+    const mockRequest = (content: string): Request => {
       const req = {
-        isAuthenticated: jest.fn(() => value),
         session: {
           userID: 'banana',
         },
@@ -114,22 +91,17 @@ describe('chat.validator', () => {
       return req as Request;
     };
 
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
     context('user가 없을 때', () => {
       it('userNotFound 메시지를 반환한다', async () => {
         userRepository.findOne = jest.fn().mockResolvedValue(undefined);
 
-        const req = mockRequest(true, 'test');
-        const res = mockResponse('resolve');
+        const req = mockRequest('test');
+        const res = mockResponse();
         const next = jest.fn();
 
-        await createThreadValidator(req, res, next);
+        await chatValidator.createThreadValidator(req, res, next);
 
-        expect(res.status).toBeCalledWith(400);
-        expect(res.send).toBeCalledWith(createChatMSG.userNotFound);
+        expect(next).toBeCalledWith({ message: CREATE_CHAT_MSG.USER_NOT_FOUND, status: 400 });
       });
     });
 
@@ -137,47 +109,44 @@ describe('chat.validator', () => {
       it('chatNotFound 메시지를 반환한다', async () => {
         chatRepository.findOne = jest.fn().mockResolvedValue(undefined);
 
-        const req = mockRequest(true, 'test');
-        const res = mockResponse('resolve');
+        const req = mockRequest('test');
+        const res = mockResponse();
         const next = jest.fn();
 
-        await createThreadValidator(req, res, next);
+        await chatValidator.createThreadValidator(req, res, next);
 
-        expect(res.status).toBeCalledWith(400);
-        expect(res.send).toBeCalledWith(createChatMSG.chatNotFound);
+        expect(next).toBeCalledWith({ message: CREATE_CHAT_MSG.CHAT_NOT_FOUND, status: 400 });
       });
     });
 
     context('content가 없을 때', () => {
       it('emptyChat 메시지를 반환한다', async () => {
-        const req = mockRequest(true, '');
-        const res = mockResponse('resolve');
+        const req = mockRequest('');
+        const res = mockResponse();
         const next = jest.fn();
 
-        await createThreadValidator(req, res, next);
+        await chatValidator.createThreadValidator(req, res, next);
 
-        expect(res.status).toBeCalledWith(400);
-        expect(res.send).toBeCalledWith(createChatMSG.emptyChat);
+        expect(next).toBeCalledWith({ message: CREATE_CHAT_MSG.EMPTY_CHAT, status: 400 });
       });
     });
 
-    context('error가 발생했을 때', () => {
+    context('error가 발생하지 않았을 때', () => {
       it('next가 실행된다', async () => {
-        const req = mockRequest(true, 'test');
-        const res = mockResponse('reject');
+        const req = mockRequest('test');
+        const res = mockResponse();
         const next = jest.fn();
 
-        await createThreadValidator(req, res, next);
+        await chatValidator.createThreadValidator(req, res, next);
 
-        expect(next).toBeCalled();
+        expect(next).toBeCalledWith();
       });
     });
   });
 
   describe('getThreadValidator', () => {
-    const mockRequest = (value: boolean): Request => {
+    const mockRequest = (): Request => {
       const req = {
-        isAuthenticated: jest.fn(() => value),
         params: {
           chatID: 1,
         },
@@ -185,34 +154,29 @@ describe('chat.validator', () => {
       return req as Request;
     };
 
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
     context('chat이 없을 때', () => {
       it('chatNotFound 메시지를 반환한다', async () => {
         chatRepository.findOne = jest.fn().mockResolvedValue(undefined);
 
-        const req = mockRequest(true);
-        const res = mockResponse('resolve');
+        const req = mockRequest();
+        const res = mockResponse();
         const next = jest.fn();
 
-        await getThreadValidator(req, res, next);
+        await chatValidator.getThreadValidator(req, res, next);
 
-        expect(res.status).toBeCalledWith(400);
-        expect(res.send).toBeCalledWith(createChatMSG.chatNotFound);
+        expect(next).toBeCalledWith({ message: CREATE_CHAT_MSG.CHAT_NOT_FOUND, status: 400 });
       });
     });
 
-    context('error가 발생했을 때', () => {
+    context('error가 발생하지 않았을 때', () => {
       it('next가 실행된다', async () => {
-        const req = mockRequest(true);
-        const res = mockResponse('reject');
+        const req = mockRequest();
+        const res = mockResponse();
         const next = jest.fn();
 
-        await getThreadValidator(req, res, next);
+        await chatValidator.getThreadValidator(req, res, next);
 
-        expect(next).toBeCalled();
+        expect(next).toBeCalledWith();
       });
     });
   });
