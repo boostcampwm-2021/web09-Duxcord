a35a7018979b6888e89c1671bcbda1252f1623f8
KIM CHAN HEE <superlipbalm@gmail.com>
2022-01-09 23:28:32 +0900

    Refactor : RestrictedRoute의 Props 인터페이스 변경 component prop 값으로 undefined 허용하지 않도록 함 Route의 component 속성에 인라인 함수 사용하던 것을 useCallback 사용해 메모이제이션

--- client/src/components/common/RestrictedRoute/index.tsx
+++ client/src/components/common/RestrictedRoute/index.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Route, RouteProps } from 'react-router-dom';
 import RestrictedComponent from './RestrictedComponent';
 
-interface Props extends RouteProps {
+interface Props extends Omit<RouteProps, 'component'> {
+  component: React.ElementType;
   signIn: boolean;
   redirectPath: string;
 }
 
 function RestrictedRoute({ component, signIn, redirectPath, ...rest }: Props) {
-  if (!component) return null;
-
-  return (
-    <Route
-      {...rest}
-      component={() => (
-        <RestrictedComponent component={component} signIn={signIn} redirectPath={redirectPath} />
-      )}
-    />
+  const Component = useCallback(
+    () => <RestrictedComponent component={component} signIn={signIn} redirectPath={redirectPath} />,
+    [component, redirectPath, signIn],
   );
+
+  return <Route {...rest} component={Component} />;
 }
 
 export default React.memo(RestrictedRoute);
