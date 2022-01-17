4ca54458009ab4bf977a79305b7028d19de92570
KIM CHAN HEE <superlipbalm@gmail.com>
2022-01-17 21:55:56 +0900

    Fix : useGroups 매개변수, useSWR 타입 수정

--- client/src/hooks/useGroups.ts
+++ client/src/hooks/useGroups.ts
@@ -1,7 +1,7 @@
 import { useEffect, useLayoutEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import useSWR from 'swr';
-import { PublicConfiguration } from 'swr/dist/types';
+import { BareFetcher, PublicConfiguration } from 'swr/dist/types';
 
 import { setSelectedGroup } from '@redux/selectedGroup/slice';
 import { API_URL, SOCKET } from '@constants/index';
@@ -20,8 +20,14 @@ export const getGroupsFetcher = async (url: string) => {
   }
 };
 
-export const useGroups = (options?: Partial<PublicConfiguration>) => {
-  const { data: groups, ...rest } = useSWR(API_URL.USER.GET_GROUPS, getGroupsFetcher, options);
+export const useGroups = (
+  options?: Partial<PublicConfiguration<GroupData[], any, BareFetcher<GroupData[]>>>,
+) => {
+  const { data: groups, ...rest } = useSWR<GroupData[], string>(
+    API_URL.USER.GET_GROUPS,
+    getGroupsFetcher,
+    options,
+  );
   const { data: userData } = useSWR(API_URL.USER.GET_USERDATA, getFetcher);
   const selectedGroup = useSelectedGroup();
   const dispatch = useDispatch();
